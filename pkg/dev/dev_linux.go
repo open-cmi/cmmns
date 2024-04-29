c8a711f0b85ff9ab097fcda819f109be055d6fa5
open-cmi <danielzhao2029@gmail.com>
2024-04-29 11:25:42 +0800

    modify device uuid get method

--- pkg/dev/dev_linux.go
+++ pkg/dev/dev_linux.go
@@ -1,28 +1,58 @@
 package dev
 
 import (
-	"io"
 	"os"
+	"os/exec"
 	"strings"
 )
 
-var deviceIDFiles []string = []string{
-	"/sys/class/dmi/id/product_uuid",
-	"/sys/block/mmcblk0/device/serial",
-}
-
 // GetDeviceID func
 func GetDeviceID() string {
-	for _, filep := range deviceIDFiles {
-		file, err := os.Open(filep)
-		if err != nil {
-			continue
-		}
-
-		data, _ := io.ReadAll(file)
-		deviceid := strings.Trim(string(data), " \r\n\t ")
-		file.Close()
-		return deviceid
+	execPath, err := os.Executable()
+	if err != nil {
+		return ""
 	}
-	return ""
+	var args []string = []string{"-h", "--output=source", execPath}
+	cmd := exec.Command("df", args...)
+	output, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
+	arr := strings.Split(string(output), "\n")
+	if len(arr) < 2 {
+		return ""
+	}
+	disk := arr[1]
+
+	var args2 []string = []string{"-o", "value", disk}
+	cmdBlkid := exec.Command("blkid", args2...)
+	output, err = cmdBlkid.Output()
+	if err != nil {
+		return ""
+	}
+	arr = strings.Split(string(output), "\n")
+	if len(arr) < 3 {
+		return ""
+	}
+	uuid := arr[0]
+	return uuid
 }
+
+// func GetDeviceID() string {
+// 	var deviceIDFiles []string = []string{
+// 		"/sys/class/dmi/id/product_uuid",
+// 		"/sys/block/mmcblk0/device/serial",
+// 	}
+// 	for _, filep := range deviceIDFiles {
+// 		file, err := os.Open(filep)
+// 		if err != nil {
+// 			continue
+// 		}
+
+// 		data, _ := io.ReadAll(file)
+// 		deviceid := strings.Trim(string(data), " \r\n\t ")
+// 		file.Close()
+// 		return deviceid
+// 	}
+// 	return ""
+// }
