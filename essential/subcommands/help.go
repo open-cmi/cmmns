9805eaf0434268a78e73dc54bcb54e976da70081
open-cmi <danielzhao2029@gmail.com>
2024-08-04 09:40:16 +0800

    update subcommand

--- essential/subcommands/help.go
+++ essential/subcommands/help.go
@@ -1,5 +1,11 @@
 package subcommands
 
+import (
+	"fmt"
+	"os"
+	"path"
+)
+
 type HelpCommand struct {
 }
 
@@ -8,7 +14,13 @@ func (c *HelpCommand) Synopsis() string {
 }
 
 func (c *HelpCommand) Run() error {
-	Usage()
+	prog := path.Base(os.Args[0])
+	fmt.Printf("Usage: %s <subcommand> <subcommand args>\n\n", prog)
+
+	fmt.Printf("Subcommands:\n")
+	for name, command := range cmdmapping {
+		fmt.Printf("\t%-15s\t%s\n", name, command.Synopsis())
+	}
 	return nil
 }
 
