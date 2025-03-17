b0bd08852179a60c3dc1d5edada4d97a972a17da
open-cmi <danielzhao2029@gmail.com>
2025-03-17 11:28:43 +0800

    add mcode

---
+++ api/system/sysinfo/mcode.go
@@ -0,0 +1,18 @@
+package sysinfo
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/open-cmi/cmmns/pkg/dev"
+	"github.com/open-cmi/cmmns/service/webserver"
+)
+
+func GetDeviceInfo(c *gin.Context) {
+	mcode := dev.GetDeviceID()
+	c.JSON(http.StatusOK, gin.H{"ret": 0, "msg": "", "data": mcode})
+}
+
+func init() {
+	webserver.RegisterUnauthAPI("system", "GET", "/mcode/", GetDeviceInfo)
+}
