88c3acad2c15b11948d02d0b42a972bacef3ad13
open-cmi <danielzhao2029@gmail.com>
2025-03-21 16:51:01 +0800

    update mac parse

--- pkg/macparse/parse.go
+++ pkg/macparse/parse.go
@@ -1,16 +1,11 @@
 package macparse
 
 import (
-	"fmt"
 	"net"
 	"strings"
 )
 
 func ParseThreeSectionMAC(macStr string) (string, error) {
-	if len(macStr) != 14 || strings.Count(macStr, "-") != 2 {
-		return "", fmt.Errorf("invalid mac format")
-	}
-
 	s := strings.ReplaceAll(macStr, "-", ".")
 	hw, err := net.ParseMAC(s)
 	if err != nil {
@@ -21,6 +16,11 @@ func ParseThreeSectionMAC(macStr string) (string, error) {
 }
 
 func ParseMAC(macStr string) (string, error) {
+
+	if len(macStr) == 14 && strings.Count(macStr, "-") == 2 {
+		return ParseThreeSectionMAC(macStr)
+	}
+
 	hw, err := net.ParseMAC(macStr)
 	if err != nil {
 		return "", err
